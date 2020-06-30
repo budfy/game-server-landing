bf5aa0e3865c79a45dac653e5cfd4972382c8522
budfy <serdyuk.ro@gmail.com>
2020-06-30 12:36:24 +0300

    Design fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,21 @@
 $(function () {
-  $('.top__tab-switcher').click(function (e) {
-    let tab = $(this).data('tab');
-    $(tab).fadeIn(500);
-    $(tab).siblings().fadeOut();
+  $('a[href^="#"]').click(function (e) {
     $('html,body').stop().animate({
-      scrollTop: ($($(this).attr('href')).offset().top - 110),
+      scrollTop: ($($(this).attr('href')).offset().top - 180),
     }, 700);
     e.preventDefault();
-  })
+  });
+
+  $('.top__tab-switcher').click(function (e) {
+    let tab = $(this).data('tab');
+    $(tab).siblings().slideUp(250);
+    setTimeout(() => {
+      $(tab).slideDown(250);
+    }, 300);
+    $('html,body').stop().animate({
+      scrollTop: ($($(this).attr('href')).offset().top - 190),
+    }, 700);
+    e.preventDefault();
+  });
+
 });
